51747747e08719d8b958c88422bbd5a9d078e0ac
Yusuph Omari <yousufummar649@gmail.com>
2023-09-20 12:36:46 +0300

    Refactoring code

--- duka/settings.py
+++ duka/settings.py
@@ -152,8 +152,8 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_USE_SSL = config('EMAIL_USE_SSL')
+EMAIL_USE_TLS =True
+EMAIL_USE_SSL = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
